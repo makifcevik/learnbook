c4be8c246bf31197235aed17d8326e496d83bdcb
makifcevik <makif.cevik02@gmail.com>
2023-11-29 17:19:40 +0300

    added flask server

---
+++ src/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/sign-up")
+def sing_up():
+    return render_template("sign-up.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
