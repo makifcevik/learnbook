e776ee798dff3b94cdb25fccf4b7b0cb2341f09f
makifcevik <makif.cevik02@gmail.com>
2023-11-29 17:44:27 +0300

    added "/" route

--- src/server.py
+++ src/server.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
 @app.route("/login")
 def login():
     return render_template("login.html")
